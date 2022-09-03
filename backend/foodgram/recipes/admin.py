fa0fcee162b9e1ce47af54cb9ceb387db6791930
Вячеслав Афанасьев <vafansjev@yandex.ru>
2022-09-03 20:14:59 +0300

    И снова третье сентября...

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import (Ingredient, Recipe, Tag, Favorite,
-                     ShoppingCart, RecipeIngredient)
+from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingCart, Tag)
 
 
 @admin.register(Ingredient)
